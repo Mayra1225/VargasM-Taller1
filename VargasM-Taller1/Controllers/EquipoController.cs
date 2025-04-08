418ef9864bbba23007f7493b55167cf6a9426bf6
Mayra <mayra.vargas@udla.edu.ec>
2025-04-08 12:34:48 -0500

    Avance Taller 1

---
+++ VargasM-Taller1/Controllers/EquipoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VargasM_Taller1.Models;
+using VargasM_Taller1.Repositories;
+
+namespace VargasM_Taller1.Controllers
+{
+    public class EquipoController : Controller
+    {
+        public ActionResult View()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            EquipoRepository repository = new EquipoRepository();
+            var equipos = repository.DevuelveListadoEquipos();
+
+            equipos = equipos.OrderBy(item => item.PartidosGanados);
+
+            return View(equipos);
+        }
+    }
+}
---
+++ VargasM-Taller1/Repositories/EquipoRepository.cs
@@ -0,0 +1,36 @@
+﻿using VargasM_Taller1.Models;
+
+namespace VargasM_Taller1.Repositories
+{
+    public class EquipoRepository
+    {
+        public IEnumerable<Equipo> DevuelveListadoEquipos()
+        {
+            List<Equipo> equipos = new List<Equipo>();
+
+            Equipo ldu = new Equipo
+            {
+                Id = 1,
+                Nombre = "Liga de Quito",
+                PartidosEmpatados = 0,
+                PartidosGanados = 10,
+                PartidosJugados = 10,
+                PartidosPerdidos = 0,
+            };
+
+            Equipo barceloina = new Equipo
+            {
+                Id = 2,
+                Nombre = "Barcelona",
+                PartidosEmpatados = 0,
+                PartidosGanados = 8,
+                PartidosJugados = 10,
+                PartidosPerdidos = 2,
+            };
+            equipos.Add(ldu);
+            equipos.Add(barceloina);
+
+            return equipos;
+        }
+    }
+}
